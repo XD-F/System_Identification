b4d63f0c6ef36f43238f2332c1a95f87b3039dee
Xuanda Feng <106212098+XD-F@users.noreply.github.com>
2022-07-18 21:43:21 +0900

    Add system_identify and NARMAX

---
+++ NARMAX_sysi.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 13:29:13 2022
+
+@author: Feng
+"""
+"""
+Introduction:
+
+This program is used to calculate the differential equation model for each order 
+of the sensor (the differential equation model is obtained from the transfer 
+function model of the sensor by Z-transformation)
+
+
+
+The data used in this program are the step response data of the humidity sensor
+in the experimental water tank.
+Please download from the following path in I3's server:
+    
+path: i3-darwin:/mnt/data/MURON/WaterVaporObservation/RawData/20220217_GC
+
+
+For more information on system identification, 
+please refer to the following information:
+    
+Paper: Lacerda et al., (2020). SysIdentPy: A Python package for System 
+Identification using NARMAX models. Journal of Open Source Software, 5(54), 2384, 
+https://doi.org/10.21105/joss.02384
+Python Package: sysidentpy https://sysidentpy.org/
+
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sysidentpy.model_structure_selection import FROLS
+from sysidentpy.basis_function import Polynomial, Fourier
+# from sysidentpy.metrics import root_relative_squared_error
+# from sysidentpy.utils.generate_data import get_siso_data
+from sysidentpy.utils.display_results import results
+# from sysidentpy.utils.plotting import plot_residues_correlation, plot_results
+# from sysidentpy.residues.residues_correlation import compute_residues_autocorrelation, compute_cross_correlation
+
+# Plot Setting
+plt.rcParams['figure.dpi'] = 300
+init_humi = 94.35
+ramp_time = 3
+
+
+# Import data
+data = pd.read_csv("./20220217_GC/07B2_20220217_GC_timeSyncData.csv", header=0, index_col=False)
+
+termi_humi = np.mean(data["humi(SHT35)"].values[54000:54300])
+
+humidity_sensor = data["humi(SHT35)"].values[53385:54048]
+humidity_true = np.append(np.linspace(init_humi, init_humi, 60),
+                    np.linspace(init_humi, termi_humi, ramp_time))
+humidity_true = np.append(humidity_true, 
+                          np.linspace(termi_humi, termi_humi, 600))
+
+
+time = np.linspace(0, len(humidity_sensor)-1, len(humidity_sensor))
+time = time
+
+x_train = np.array([humidity_true[60:-1]])
+y_train = np.array([humidity_sensor[60:-1]])
+x_train = x_train.T
+y_train = y_train.T
+
+
+
+
+### System Identification ###
+
+# Model 1
+basis_function = Polynomial(degree=1)
+model_1 = FROLS(
+    basis_function=basis_function,
+    order_selection=True,
+    n_info_values=10,
+    extended_least_squares=False,
+    ylag=1, xlag=1,
+    info_criteria='aic',
+    estimator='least_squares',
+)
+
+model_1.fit(X=x_train,y=y_train)
+result_1 = model_1.predict(x_train, y_train)
+
+r = pd.DataFrame(
+results(
+    model_1.final_model, model_1.theta, model_1.err,
+    model_1.n_terms, err_precision=8, dtype='sci'
+    ),
+columns=['Regressors', 'Parameters', 'ERR'])
+print(r)
+
+
+
+# Model 2
+basis_function = Polynomial(degree=1)
+model_2 = FROLS(
+    basis_function=basis_function,
+    order_selection=True,
+    n_info_values=10,
+    extended_least_squares=False,
+    ylag=2, xlag=2,
+    info_criteria='aic',
+    estimator='least_squares',
+)
+
+model_2.fit(X=x_train,y=y_train)
+result_2 = model_2.predict(x_train, y_train)
+
+
+# Model 3
+basis_function = Polynomial(degree=3)
+model_3 = FROLS(
+    basis_function=basis_function,
+    order_selection=True,
+    n_info_values=15,
+    extended_least_squares=False,
+    ylag=2, xlag=2,
+    info_criteria='aic',
+    estimator='least_squares',
+)
+
+model_3.fit(X=x_train,y=y_train)
+result_3 = model_3.predict(x_train, y_train)
+
+r = pd.DataFrame(
+results(
+    model_3.final_model, model_3.theta, model_3.err,
+    model_3.n_terms, err_precision=8, dtype='sci'
+    ),
+columns=['Regressors', 'Parameters', 'ERR'])
+print(r)
+
+xaxis = np.arange(1, model_3.n_info_values + 1)
+plt.plot(xaxis, model_3.info_values)
+plt.xlabel('n_terms')
+plt.ylabel('Information Criteria')
+plt.show()
+
+
+# Model 4
+basis_function = Polynomial(degree=2)
+model_4 = FROLS(
+    basis_function=basis_function,
+    order_selection=True,
+    n_info_values=10,
+    extended_least_squares=False,
+    ylag=2, xlag=2,
+    info_criteria='aic',
+    estimator='least_squares',
+    n_terms=6
+)
+
+model_4.fit(X=x_train,y=y_train)
+result_4 = model_4.predict(x_train, y_train)
+
+r = pd.DataFrame(
+results(
+    model_4.final_model, model_4.theta, model_4.err,
+    model_4.n_terms, err_precision=8, dtype='sci'
+    ),
+columns=['Regressors', 'Parameters', 'ERR'])
+print(r)
+
+
+# Model 5
+basis_function = Polynomial(degree=4)
+model_5 = FROLS(
+    basis_function=basis_function,
+    order_selection=True,
+    n_info_values=15,
+    extended_least_squares=False,
+    ylag=3, xlag=3,
+    info_criteria='aic',
+    estimator='least_squares'
+)
+
+model_5.fit(X=x_train,y=y_train)
+result_5 = model_5.predict(x_train, y_train)
+
+r = pd.DataFrame(
+results(
+    model_5.final_model, model_5.theta, model_5.err,
+    model_5.n_terms, err_precision=8, dtype='sci'
+    ),
+columns=['Regressors', 'Parameters', 'ERR'])
+print(r)
+
+
+
+# Model 6
+basis_function = Polynomial(degree=2)
+model_6 = FROLS(
+    basis_function=basis_function,
+    order_selection=True,
+    n_info_values=10,
+    extended_least_squares=False,
+    ylag=2, xlag=2,
+    info_criteria='aic',
+    estimator='least_squares'
+)
+
+model_6.fit(X=x_train,y=y_train)
+result_6 = model_6.predict(x_train, y_train)
+
+r = pd.DataFrame(
+results(
+    model_6.final_model, model_6.theta, model_6.err,
+    model_6.n_terms, err_precision=8, dtype='sci'
+    ),
+columns=['Regressors', 'Parameters', 'ERR'])
+print(r)
+
+### Plot ###
+
+plt.Figure()
+plt.plot(time[60:-1], humidity_true[60:-1], label="Enviroment")
+plt.plot(time[60:-1], humidity_sensor[60:-1], label="sensor")
+plt.plot(time[61:-1], result_1[0:-1], label="first order")
+plt.plot(time[61:-1], result_2[0:-1], '--', markersize=3, label="second order")
+plt.plot(time[61:-1], result_6[0:-1], '1', markersize=3, label="second order with Nonlinearity")
+plt.plot(time[61:-1], result_3[0:-1], '*', markersize=3, label="third order with Nonlinearity")
+plt.plot(time[61:-1], result_4[0:-1], 'o', markersize=3, label="second order with Order Reduction")
+plt.plot(time[61:-1], result_5[0:-1], '^', markersize=3, label="third order with forth order Nonlinearity")
+
+plt.xlabel("Time / s")
+plt.ylabel("Humidity / %")
+plt.legend()
+plt.title("True humidity(Input) and Sensor(Output)")
+plt.show()
+
+
+
+
+plt.Figure()
+plt.plot(time[80:260], humidity_true[80:260], label="Enviroment")
+plt.plot(time[80:260], humidity_sensor[80:260], label="sensor")
+plt.plot(time[80:260], result_1[20:200], label="first order")
+plt.plot(time[80:260], result_2[20:200], '--', markersize=3, label="second order")
+plt.plot(time[80:260], result_6[20:200], '1', markersize=3, label="second order with Nonlinearity")
+plt.plot(time[80:260], result_3[20:200], '*', markersize=3, label="third order with Nonlinearity")
+plt.plot(time[80:260], result_4[20:200], 'o', markersize=3, label="second order with Order Reduction")
+plt.plot(time[80:260], result_5[20:200], '^', markersize=3, label="third order with forth order Nonlinearity")
+
+plt.xlabel("Time / s")
+plt.ylabel("Humidity / %")
+plt.legend()
+plt.title("True humidity(Input) and Sensor(Output)")
+plt.show()
+
+
+plt.Figure()
+plt.plot(time[-200:-1], humidity_true[-200:-1], label="Enviroment")
+plt.plot(time[-200:-1], humidity_sensor[-200:-1], label="sensor")
+plt.plot(time[-200:-1], result_1[-200:-1], label="first order")
+plt.plot(time[-200:-1], result_2[-200:-1], '--', markersize=3, label="second order")
+plt.plot(time[-200:-1], result_6[-200:-1], '1', markersize=3, label="second order with Nonlinearity")
+plt.plot(time[-200:-1], result_3[-200:-1], '*', markersize=3, label="third order with Nonlinearity")
+plt.plot(time[-200:-1], result_4[-200:-1], 'o', markersize=3, label="second order with Order Reduction")
+plt.plot(time[-200:-1], result_5[-200:-1], '^', markersize=3, label="third order with forth order Nonlinearity")
+
+plt.xlabel("Time / s")
+plt.ylabel("Humidity / %")
+plt.legend()
+plt.title("True humidity(Input) and Sensor(Output)")
+plt.show()
+
+
+
+
+# plt.Figure()
+# plt.plot(humidity_true[282:300], label="Enviroment")
+# plt.plot(humidity_sensor[282:300], label="sensor")
+# plt.plot(result_5[282:300], '^', markersize=3, label="Third Order")
+
+# plt.xlabel("Time / s")
+# plt.ylabel("Humidity / %")
+# plt.legend()
+# plt.title("True humidity(Input) and Sensor(Output)")
+# plt.show()
+
---
+++ SystemIdentify.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 11:37:45 2022
+
+@author: Feng
+"""
+
+"""
+Introduction: 
+    This program is used to model the transfer function of the presumed humidity sensor.
+
+Model:
+    
+    1.  
+        Curve Fitting with Least Squares Method
+        https://docs.scipy.org/doc/scipy-1.8.0/html-scipyorg/reference/generated/scipy.optimize.curve_fit.html#scipy.optimize.curve_fit
+    
+    2. 
+        Non-linear Autoregressive Models with Moving Average and Exogenous Input
+        http://sysidentpy.org/introduction_to_narmax.html
+    
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as optimization
+
+# Plot Setting
+plt.rcParams['figure.dpi'] = 300 # Set the output image resolution
+init_humi = 100.51               # Set initial humidity before step
+ramp_time = 3                    # Set time length of ramp
+   
+# Import data
+data = pd.read_csv("./AppARIA_201BA8A_TWELITE-ARIA_20220217.csv", header=0, index_col=False)
+
+termi_humi = np.mean(data["humi(BA8A)"].values[2300:2600])
+
+humidity_sensor = data["humi(BA8A)"].values[1632:2295]
+humidity_true = np.append(np.linspace(init_humi, init_humi, 60),
+                    np.linspace(init_humi, termi_humi, ramp_time))
+humidity_true = np.append(humidity_true, 
+                          np.linspace(termi_humi, termi_humi, 600))
+
+timestamp = np.linspace(0, len(humidity_sensor)-1, len(humidity_sensor))
+timestamp = timestamp * 5
+
+
+
+# Fit Curve
+
+bias = np.mean(humidity_sensor[-101:-1])
+
+fitdata = humidity_sensor[60:-1]
+time = np.arange(0, len(fitdata), 1)
+
+
+# T -- Time Constant of first order system
+def first_order_system(t, T):
+    return bias + (init_humi-bias) * np.exp(-t/T)
+popt1, povc1 = optimization.curve_fit(first_order_system, xdata=time, ydata=fitdata,
+                                      p0=[8])
+first_order_output = first_order_system(time, popt1[0])
+
+
+# T1, T2 -- Parameters of second order system
+def second_order_system(t, sigma, wn):
+    T1 = 1 / (wn * (sigma - (sigma**2 - 1)**0.5))
+    T2 = 1 / (wn * (sigma + (sigma**2 - 1)**0.5))
+    return bias + (init_humi-bias)*(T1-T2)/(T1+T2) * (- np.exp(-t/T1) / (T2/T1 - 1) + np.exp(-t/T2) / (T1/T2 - 1))
+popt2, povc2 = optimization.curve_fit(second_order_system, xdata=time, ydata=fitdata,
+                                      p0=[10, 1])
+second_order_output = second_order_system(time, popt2[0], popt2[1])
+
+
+# Plot
+plt.Figure()
+plt.plot(timestamp[60:-1], humidity_true[60:-1], label="Enviroment")
+plt.plot(timestamp[60:-1], humidity_sensor[60:-1], label="sensor")
+plt.plot(timestamp[60:-2], first_order_output[0:-1], 'o', markersize=3, label="first_order")
+plt.plot(timestamp[60:-2], second_order_output[0:-1],'--', markersize=3, label="second_order")
+plt.xlabel("Time / s")
+plt.ylabel("Humidity / %")
+plt.legend()
+plt.title("True humidity(Input) and Sensor(Output)")
+plt.show()
+
+
+plt.Figure()
+plt.plot(timestamp[60:120], humidity_true[60:120], label="Enviroment")
+plt.plot(timestamp[60:120], humidity_sensor[60:120], label="sensor")
+plt.plot(timestamp[60:120], first_order_output[0:60], 'o', markersize=3, label="first_order")
+plt.plot(timestamp[60:120], second_order_output[0:60],'--', markersize=3, label="second_order")
+plt.xlabel("Time / s")
+plt.ylabel("Humidity / %")
+plt.legend()
+plt.title("True humidity(Input) and Sensor(Output)")
+plt.show()
+
+
+plt.Figure()
+plt.plot(timestamp[-61:-1], humidity_true[-61:-1], label="Enviroment")
+plt.plot(timestamp[-61:-1], humidity_sensor[-61:-1], label="sensor")
+plt.plot(timestamp[-61:-1], first_order_output[-61:-1], 'o', markersize=3, label="first_order")
+plt.plot(timestamp[-61:-1], second_order_output[-61:-1],'--', markersize=3, label="second_order")
+plt.xlabel("Time / s")
+plt.ylabel("Humidity / %")
+plt.legend()
+plt.title("True humidity(Input) and Sensor(Output)")
+plt.show()
